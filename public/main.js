93ac6c057d38210fdbdfc0ed33e7a71482ebf9c6
Cesar Mac <rasec_1113@hotmail.com>
2016-04-09 11:01:37 -0500

    array functions. ES2015

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 "use strict";
 // self executing function here
-(function() {
+(() => {
 	const PRIMARY_UNORDERED_LIST = 'primary-options',
 		HAS_CHILDREN = 'has-children',
 		SECONDARY_UNORDERED_LIST = 'secondary-options',
@@ -9,61 +9,59 @@
 		ACTIVE_CLASS = 'active',
 		MENU_SHOWED = 'menu-showed',
 		SUBMENU_SHOWED = 'submenu-showed';
-	var cacheElements = {
+	var cacheEl = {
 		primarySelected: null,
-		navContent: null,
+		navContentStyle: null,
 		body: null
 	};
 	var Util = {
-		isMobile: () => cacheElements.navContentStyle.display === 'block',
-		addClass: (...args) => {
-			var element = Array.prototype.shift.call(args, 1);
-			element.classList.add(args);
+		isMobile: () => cacheEl.navContentStyle.display === 'block',
+		addClass: (element, className) => {
+			element.classList.add(className);
 		},
-		removeClass: (...args) => {
-			var element = Array.prototype.shift.call(args, 1);
-			element.classList.remove(args);
+		removeClass: (element, className) => {
+			element.classList.remove(className);
 		}
 	}
 
 	function loadFn() {
-		saveElementsCached();
-		processBody();
-		processLogo();
-		processCopyright();
+		processElements();
 		Nav.load();
 	}
 
-	function saveElementsCached() {
-		cacheElements.navContentStyle = getComputedStyle(document.getElementsByClassName("nav-content")[0]);
-	}
+	function processElements() {
+		function processBody() {
+			let body = cacheEl.body;
+			body.locked = (className = MENU_SHOWED) => {
+				Util.addClass(body, className);
+			};
+			body.unlocked = (className = MENU_SHOWED) => {
+				Util.removeClass(body, className);
+			};
+			document.getElementById('mask').addEventListener('click', Nav.hideAll);
+		}
 
-	function processBody() {
-		let body = cacheElements.body = document.body;
-		body.locked = function(className = MENU_SHOWED) {
-			Util.addClass(body, className);
+		function processLogo() {
+			document.getElementById('huge-logo').addEventListener('click', Nav.hideAll);
 		};
-		body.unlocked = function(className = MENU_SHOWED) {
-			Util.removeClass(body, className);
-		};
-		document.getElementById('mask').addEventListener('click', Nav.hideAll);
-	}
 
-	function processLogo() {
-		document.getElementById('huge-logo').addEventListener('click', Nav.hideAll);
-	};
-
-	function processCopyright() {
-		var currentYear = new Date().getFullYear();
-		document.getElementById('copyright').firstElementChild.innerHTML = currentYear;
+		function processCopyright() {
+			let currentYear = new Date().getFullYear();
+			document.getElementById('copyright').firstElementChild.innerHTML = currentYear;
+		}
+		cacheEl.navContentStyle = getComputedStyle(document.getElementsByClassName("nav-content")[0]);
+		cacheEl.body = document.body;
+		processBody();
+		processLogo();
+		processCopyright();
 	}
 	var Nav = {
 		load: () => {
-			Nav.get().then(Nav.build.bind(Nav)).catch(errorMsg => console.error(errorMsg));
+			Nav.get().then(Nav.build).catch(errorMsg => console.error(errorMsg));
 		},
 		get: () => {
 			return new Promise((resolve, reject) => {
-				var xmlhttp = new XMLHttpRequest();
+				let xmlhttp = new XMLHttpRequest();
 				xmlhttp.open("GET", './api/nav.json');
 				xmlhttp.onreadystatechange = () => {
 					if (xmlhttp.readyState == XMLHttpRequest.DONE) {
@@ -78,28 +76,28 @@
 			});
 
 		},
-		createOption: function(data, secondary) {
-			var item = document.createElement('li');
+		createOption: (data, secondary = false) => {
+			let item = document.createElement('li');
 			item.innerHTML = '<a class="' + (secondary ? SECONDARY_ANCHOR : PRIMARY_ANCHOR) + '" href="' + data.url + '">' + data.label + '</a>';
 			return item;
 		},
-		build: function(iarItems) {
-			var itemData,
+		build: (items) => {
+			let itemData,
 				item,
 				children,
 				childrenNumber,
 				subMenu,
 				navOptions = document.getElementsByClassName(PRIMARY_UNORDERED_LIST)[0];
-			for (var i = 0; i < iarItems.length; i++) {
-				itemData = iarItems[i];
+			for (let i = 0; i < items.length; i++) {
+				itemData = items[i];
 				children = itemData.items;
 				childrenNumber = children.length;
-				item = this.createOption(itemData);
+				item = Nav.createOption(itemData);
 				if (childrenNumber > 0) {
 					subMenu = document.createElement('ul');
 					Util.addClass(subMenu, SECONDARY_UNORDERED_LIST);
-					for (var j = 0; j < children.length; j++) {
-						subMenu.appendChild(this.createOption(children[j], true));
+					for (let j = 0; j < children.length; j++) {
+						subMenu.appendChild(Nav.createOption(children[j], true));
 					}
 					item.appendChild(subMenu);
 					Util.addClass(item, HAS_CHILDREN);
@@ -110,52 +108,55 @@
 			navOptions.addEventListener('keyup', Events.onKeyupNav.bind(Events));
 			document.getElementById('menu-mobile').addEventListener('click', Events.onMenuMobileClick);
 		},
-		hideAll: function() {
+		hideAll: () => {
 			Nav.hideSubMenu(true);
-			cacheElements.body.unlocked();
-			cacheElements.body.unlocked(SUBMENU_SHOWED);
+			cacheEl.body.unlocked();
+			cacheEl.body.unlocked(SUBMENU_SHOWED);
 		},
-		hideSubMenu: function(removeMask) {
-			if (cacheElements.primarySelected) {
-				Util.removeClass(cacheElements.primarySelected, ACTIVE_CLASS);
-				delete cacheElements.primarySelected;
+		hideSubMenu: (removeMask = false) => {
+			if (cacheEl.primarySelected) {
+				Util.removeClass(cacheEl.primarySelected, ACTIVE_CLASS);
+				delete cacheEl.primarySelected;
 			}
 			if (removeMask) {
 				if (Util.isMobile()) {
-					cacheElements.body.locked();
+					cacheEl.body.locked();
 				}
-				cacheElements.body.unlocked(SUBMENU_SHOWED);
+				cacheEl.body.unlocked(SUBMENU_SHOWED);
 			}
 		},
-		activateSubMenu: function(primaryNav) {
+		activateSubMenu: (primaryNav) => {
+			if (!primaryNav)
+				return;
 			let primaryItem = primaryNav.parentNode,
 				hasSubMenu = !!primaryNav.nextSibling,
 				normalBehaivor = !hasSubMenu;
 			//clicked the same primary nav
-			if (cacheElements.primarySelected == primaryItem) {
-				this.hideSubMenu(true);
+			if (cacheEl.primarySelected == primaryItem) {
+				Nav.hideSubMenu(true);
 				return normalBehaivor;
 			}
-			this.hideSubMenu(normalBehaivor);
+			Nav.hideSubMenu(normalBehaivor);
 			if (hasSubMenu) {
 				Util.addClass(primaryItem, ACTIVE_CLASS);
-				cacheElements.primarySelected = primaryItem;
-				cacheElements.body.locked(SUBMENU_SHOWED);
+				cacheEl.primarySelected = primaryItem;
+				cacheEl.body.locked(SUBMENU_SHOWED);
 			}
 			return normalBehaivor;
 		},
-		toggleMobileMenu: function() {
-			let classList = cacheElements.body.classList;
+		toggleMobileMenu: () => {
+			let body = cacheEl.body,
+				classList = body.classList;
 			if (classList.contains(MENU_SHOWED) || classList.contains(SUBMENU_SHOWED)) {
-				this.hideAll();
+				Nav.hideAll();
 			} else {
-				cacheElements.body.locked();
+				body.locked();
 			}
 		}
 	};
 	var Events = {
-		onClickNav: function(event) {
-			var el = event.target;
+		onClickNav: (event) => {
+			let el = event.target;
 			if (el.tagName == 'A') {
 				if (el.classList.contains(PRIMARY_ANCHOR)) {
 					if (!Nav.activateSubMenu(el)) {
@@ -167,16 +168,16 @@
 				}
 			}
 		},
-		onKeyupNav: function(event) {
+		onKeyupNav: (event) => {
 			console.warn('Experimental test');
-			var el = event.target;
+			let el = event.target;
 			event.preventDefault();
 			if (event.keyCode == 13) {
-				this.onClickNav(event);
+				Events.onClickNav(event);
 			}
 			return false;
 		},
-		onMenuMobileClick: function(event) {
+		onMenuMobileClick: (event) => {
 			event.preventDefault();
 			Nav.toggleMobileMenu();
 		}
