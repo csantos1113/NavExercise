601005dec7f897b68e2982e09e7b96b0d59c14b1
unknown <csantos@os005694.open.com.co>
2016-04-11 13:31:25 +0100

    fix generating css on Windows machines

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ var express = require('express'),
 	path = require('path'),
 	api = require('./api'),
 	app = express(),
-	publicDir = path.join(__dirname, 'public');
+	publicDir = path.join(__dirname, 'public'),
+	css_file = path.join(publicDir, 'styles', 'main.css');
+
+var fs = require('fs'),
+	sass = require('node-sass'),
+	result = sass.renderSync({
+		file: path.join(__dirname, 'sass', 'main.scss'),
+		outputStyle: 'compact'
+	});
+console.log("Rendering Complete, saving .css file...");
+fs.writeFileSync(css_file, result.css.toString());
+console.log("Wrote CSS to " + css_file);
 
 app.use(logger('dev'));
 app.use(express.static(publicDir));
